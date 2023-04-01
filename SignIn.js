257eb7a81788bb42b6d7d682dd0a3f2978ac48ba
AldrikDeJongh <126321462+AldrikDeJongh@users.noreply.github.com>
2023-04-01 16:19:16 +0200

    Update SignIn.js

--- SignIn.js
+++ SignIn.js
@@ -1,1 +1,8 @@
 // This function enters a new user
+function SignIn() {
+  var firstName = prompt("Please enter your First Name:");
+  var lastName = prompt("Please enter your Last Name:");
+  
+  // Do something with the first and last name variables, for example:
+  console.log("Welcome, " + firstName + " " + lastName + "!");
+}
